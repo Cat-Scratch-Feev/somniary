e0a04adcdb81c622b7714fd54f7e04b97d077b3b
josielynngriffin <jelynngriffin@gmail.com>
2023-06-05 23:26:21 -0400

    Add dependency imports, starter router code
    Group contributions on LiveShare during initial production

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const dreamRoutes = require('./dreamRoutes');
+const tagRoutes = require('./tagRoutes');
+
+router.use('/users', categoryRoutes);
+router.use('/dreams', productRoutes);
+router.use('/tags', tagRoutes);
+
+module.exports = router;
