967f2839580fd1409091ed5d139e69abdd7642e1
James Farris <jafarris.exe@gmail.com>
2023-06-07 19:50:31 -0400

    Added basic template for main page

--- server.js
+++ server.js
@@ -1,32 +1,40 @@
 //imports of dependency variables
-const path = require('path');
-const express= require('express');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const sequelize = require('./config/connection.js');
-const routes = require('./controllers');
+const sequelize = require("./config/connection.js");
+const routes = require("./controllers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 //session and cookies
 const sess = {
-    secret: 'secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize,
-    }),
-  };
-  
-  app.use(session(sess));
+  secret: "secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+// Handlebars integration
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}/`));
-  });
+  app.listen(PORT, () =>
+    console.log(`Now listening at http://localhost:${PORT}/`)
+  );
+});
