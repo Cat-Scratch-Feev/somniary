ab5b54347545cba0007e4a2e483b4b22e0c6b5f8
XyrillaSC <professionalstevenkramer@gmail.com>
2023-06-07 20:19:28 -0400

    added user api crud route/update dream modal

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,84 @@
+const router = require('express').Router();
+const { User, Dream } = require('../../models');
+
+// The `/api/users` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  try {
+    const userData = await User.findAll({
+      include: [{ model: Dream }],
+    });
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find one user by its `id` value
+  try {
+    const userData = await User.findByPk(req.params.id, {include: [{ model: Dream }]});
+    if (!userData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new user
+  try {
+    const userData = await User.create({
+      email: req.body.email,
+      password: req.body.password,
+      username: req.body.username,
+    });
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  try {
+    const userData = await User.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!userData[0]) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const userData = await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with that id!' });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- models/Dream.js
+++ models/Dream.js
@@ -23,6 +23,11 @@ Dream.init(
             type: DataTypes.DATE,
             allowNull: false
         },
+        is_shared: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+        },
         user_id: {
             type: DataTypes.INTEGER,
             reference: {
