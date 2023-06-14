630719e66bd0cdc3a232959b9d637a80ccde7366
josielynngriffin <jelynngriffin@gmail.com>
2023-06-14 15:56:32 -0400

    Refactor code to target all classes in nav bars so that routing works across all pages

--- public/js/home.js
+++ public/js/home.js
@@ -21,27 +21,35 @@ settingsBtn.addEventListener("click", () => {
 });
 
 // About button
-const aboutBtn = document.getElementById("about_btn");
-aboutBtn.addEventListener("click", () => {
-  window.location.href = "/about";
-});
-
+const aboutBtn = document.querySelectorAll(".about_btn");
+//Use querySelectorAll to get a nodelist of elements using the specified class
+for  (i =0; i < aboutBtn.length; i++){
+  //iterate over each index of the nodelist, adding event listener to each. Adds the event to all instances of the class, meaning the routes work in the normal navigation as well as the mobile navigation.
+  aboutBtn[i].addEventListener("click", () => {
+    window.location.href = "/about";
+  });
+};
 // Journal button
-const journalBtn = document.getElementById("journal_btn");
-journalBtn.addEventListener("click", () => {
-  window.location.href = "/journal";
-});
-
+const journalBtn = document.querySelectorAll(".journal_btn");
+for  (i =0; i < journalBtn.length; i++){
+  journalBtn[i].addEventListener("click", () => {
+    window.location.href = "/journal";
+  });
+};
 // New dream button
-const dreamlogBtn = document.getElementById("dreamlog_btn");
-dreamlogBtn.addEventListener("click", () => {
-  window.location.href = "/dreamlog";
-});
-
+const dreamlogBtn = document.querySelectorAll(".dreamlog_btn");
+for  (i =0; i < dreamlogBtn.length; i++){
+  dreamlogBtn[i].addEventListener("click", () => {
+    window.location.href = "/dreamlog";
+  });
+};
 // Calendar button
-const calendarBtn = document.getElementById("calendar_btn");
-calendarBtn.addEventListener("click", () => {
-  window.location.href = "/calendar";
-});
-
+const calendarBtn = document.querySelectorAll(".calendar_btn");
+for  (i =0; i < calendarBtn.length; i++){
+  calendarBtn[i].addEventListener("click", () => {
+    console.log('test');
+    console.log(calendarBtn[i]);
+    window.location.href = "/calendar";
+  });
+};
 document.querySelector("#logout_btn").addEventListener("click", logout);
