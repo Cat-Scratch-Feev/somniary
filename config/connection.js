f4e4a1e1b78bc7675adfc33087f23090e02312a6
Mauvonte Roberts <moeroberts183@gmail.com>
2023-06-21 18:59:38 -0400

    fixed code in connection.js for jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,23 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
+require('dotenv').config(); 
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  // Use JAWSDB on Heroku
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // Use local MySQL
+  sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306,
     }
   );
-  
-  module.exports = sequelize;
-  
+}
+
+module.exports = sequelize;
